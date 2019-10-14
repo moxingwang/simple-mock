ad895e95316c0d9149d836fef427cf3b383f5f6d
xingwang.mo <xingwang.mo@chinaredstar.com>
2019-10-14 14:24:58 +0800

    11

---
+++ simple-mock-client/src/test/java/top/moxingwang/simplemock/UserService.java
@@ -0,0 +1,7 @@
+package top.moxingwang.simplemock;
+
+public class UserService {
+    public void userInfo(){
+        System.out.println("user info ");
+    }
+}
---
+++ simple-mock-client/src/test/java/top/moxingwang/simplemock/UserServiceTest.java
@@ -0,0 +1,8 @@
+package top.moxingwang.simplemock;
+
+public class UserServiceTest {
+    public static void main(String[] args) {
+        UserService userService = new UserService();
+        userService.userInfo();
+    }
+}
