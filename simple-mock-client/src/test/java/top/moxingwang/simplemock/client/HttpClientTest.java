b85ebad9967469e9fbd21cf7974d89641d78d122
xingwang.mo <xingwang.mo@chinaredstar.com>
2019-10-14 17:55:52 +0800

    commit

--- simple-mock-client/src/test/java/top/moxingwang/simplemock/client/HttpClientTest.java
+++ simple-mock-client/src/test/java/top/moxingwang/simplemock/client/HttpClientTest.java
@@ -1,13 +1,10 @@
 package top.moxingwang.simplemock.client;
 
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
 public class HttpClientTest {
     public static void main(String[] args) {
         try {
-            String str = EntityUtils.toString(HttpClients.createDefault().execute(new HttpGet("https://www.baidu.com/")).getEntity(), "UTF-8");
+            String str = org.apache.http.util.EntityUtils.toString(org.apache.http.impl.client.HttpClients.createDefault().execute(new org.apache.http.client.methods.HttpGet("https://www.baidu.com/")).getEntity(), "UTF-8");
             System.out.println(str);
         } catch (Exception e) {
 
