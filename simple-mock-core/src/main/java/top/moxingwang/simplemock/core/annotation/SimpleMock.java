92663b636a79c825bd0f6ac48c4f12ce0d022ed3
xingwang.mo <xingwang.mo@chinaredstar.com>
2019-10-15 11:31:49 +0800

    commit

--- simple-mock-core/src/main/java/top/moxingwang/simplemock/core/annotation/SimpleMock.java
+++ simple-mock-core/src/main/java/top/moxingwang/simplemock/core/annotation/SimpleMock.java
@@ -5,7 +5,7 @@ import java.lang.annotation.*;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE,ElementType.METHOD,ElementType.TYPE_USE})
-public @interface SPI {
+public @interface SimpleMock {
     /**
      * default extension name
      */
