f80e69ab80e87fe30f756144a2b5845b7340e251
xingwang.mo <xingwang.mo@chinaredstar.com>
2019-10-15 12:59:40 +0800

    commit

--- simple-mock-test/src/main/java/top/moxingwang/simplemock/test/service/HttpClientTest.java
+++ simple-mock-test/src/main/java/top/moxingwang/simplemock/test/service/HttpClientTest.java
@@ -1,12 +1,10 @@
 package top.moxingwang.simplemock.test.service;
 
 
-import top.moxingwang.simplemock.core.SimpleMockConstant;
-
 public class HttpClientTest {
     public static String main(String[] args) {
         try {
-            String str = org.apache.http.util.EntityUtils.toString(org.apache.http.impl.client.HttpClients.createDefault().execute(new org.apache.http.client.methods.HttpGet(System.getProperty(SimpleMockConstant.SIMPLE_MOCK_VM_SERVER_URL))).getEntity(), "UTF-8");
+            String str = org.apache.http.util.EntityUtils.toString(org.apache.http.impl.client.HttpClients.createDefault().execute(new org.apache.http.client.methods.HttpGet(System.getProperty(top.moxingwang.simplemock.core.SimpleMockConstant.SIMPLE_MOCK_VM_SERVER_URL))).getEntity(), "UTF-8");
             System.out.println(str);
             if (null != str && str.length() > 0) {
                 return str;
