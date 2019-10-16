04c2f46eaf64ccdfd2716da208678ea87282569a
xingwang.mo <xingwang.mo@chinaredstar.com>
2019-10-16 10:00:02 +0800

    commit

--- simple-mock-core/src/main/java/top/moxingwang/simplemock/core/SimpleMockConstant.java
+++ simple-mock-core/src/main/java/top/moxingwang/simplemock/core/SimpleMockConstant.java
@@ -1,5 +1,5 @@
 package top.moxingwang.simplemock.core;
 
 public final class SimpleMockConstant {
-    public static final String SIMPLE_MOCK_VM_SERVER_URL = "https://www.baidu.com";
+    public static final String SIMPLE_MOCK_VM_SERVER_URL = "SIMPLE_MOCK_VM_SERVER_URL";
 }
--- simple-mock-test/src/main/java/top/moxingwang/simplemock/test/service/ThreadTest.java
+++ simple-mock-test/src/main/java/top/moxingwang/simplemock/test/service/ThreadTest.java
@@ -1,22 +1,29 @@
 package top.moxingwang.simplemock.test.service;
 
+import top.moxingwang.simplemock.core.SimpleMockConstant;
 import top.moxingwang.simplemock.core.dto.MethodSpiResponseDTO;
 
 import java.lang.reflect.Method;
 
 public class ThreadTest {
     public static void main(String[] args) {
-        new ThreadTest().getInfo();
+        System.getProperties().setProperty(SimpleMockConstant.SIMPLE_MOCK_VM_SERVER_URL, "http://localhost:8080/simple-mock/mock/string/");
+        String info = new ThreadTest().getInfo();
+        System.out.println("结果"+info);
     }
 
-    public Integer getInfo() {
-        MethodSpiResponseDTO info2 = getInfo2(Thread.currentThread().getStackTrace()[1]);
+    public String getInfo() {
+        MethodSpiResponseDTO mockResponse = mock(Thread.currentThread().getStackTrace()[1]);
+        if (mockResponse.getType() == 2) {
+            return;
+        }
+
         System.out.println(1);
-//        return "OK";
-        return 1;
+        return "OK";
+//        return 1;
     }
 
-    public MethodSpiResponseDTO getInfo2(StackTraceElement stackTraceElement) {
+    public MethodSpiResponseDTO mock(StackTraceElement stackTraceElement) {
         MethodSpiResponseDTO responseDTO = new MethodSpiResponseDTO(0);
         try {
             Class cl = Class.forName(stackTraceElement.getClassName());
@@ -24,24 +31,32 @@ public class ThreadTest {
                 //调用mock server
 
                 if (method.getName().equals(stackTraceElement.getMethodName())) {
+                    String mockUrl = System.getProperty(top.moxingwang.simplemock.core.SimpleMockConstant.SIMPLE_MOCK_VM_SERVER_URL) + method.getClass().getName() + "." + method.getName();
+                    String str = org.apache.http.util.EntityUtils.toString(org.apache.http.impl.client.HttpClients.createDefault().execute(new org.apache.http.client.methods.HttpGet(mockUrl)).getEntity(), "UTF-8");
+
+
                     String returnMethodName = method.getReturnType().getName();
                     if ("void".equals(returnMethodName)) {
                         responseDTO.setType(1);
                         return responseDTO;
-                    }else if ("int".equals(returnMethodName) || "java.lang.Integer".equals(returnMethodName)) {
+                    } else if ("int".equals(returnMethodName) || "java.lang.Integer".equals(returnMethodName)) {
                         responseDTO.setType(1);
                         return responseDTO;
-                    }else if ("int".equals(returnMethodName) || "java.lang.Integer".equals(returnMethodName)) {
+                    } else if ("int".equals(returnMethodName) || "java.lang.Integer".equals(returnMethodName)) {
                         responseDTO.setType(1);
                         return responseDTO;
-                    }else if ("int".equals(returnMethodName) || "java.lang.Integer".equals(returnMethodName)) {
+                    } else if ("int".equals(returnMethodName) || "java.lang.Integer".equals(returnMethodName)) {
                         responseDTO.setType(1);
                         return responseDTO;
-                    }else if ("int".equals(returnMethodName) || "java.lang.Integer".equals(returnMethodName)) {
+                    } else if ("int".equals(returnMethodName) || "java.lang.Integer".equals(returnMethodName)) {
                         responseDTO.setType(1);
                         return responseDTO;
-                    }else if ("int".equals(returnMethodName) || "java.lang.Integer".equals(returnMethodName)) {
+                    } else if ("int".equals(returnMethodName) || "java.lang.Integer".equals(returnMethodName)) {
                         responseDTO.setType(1);
+                        return responseDTO;
+                    }else if ("java.lang.String".equals(returnMethodName)) {
+                        responseDTO.setType(1);
+                        responseDTO.setResponse(str);
                         return responseDTO;
                     }
                     System.out.println(method.getName());
