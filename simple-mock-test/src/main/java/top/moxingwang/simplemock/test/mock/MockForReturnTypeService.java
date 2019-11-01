df11fd24ed60c3f4a32df7fdbf26131296624596
xingwang.mo <xingwang.mo@chinaredstar.com>
2019-11-01 19:12:54 +0800

    commit

--- simple-mock-test/src/main/java/top/moxingwang/simplemock/test/mock/MockForReturnTypeService.java
+++ simple-mock-test/src/main/java/top/moxingwang/simplemock/test/mock/MockForReturnTypeService.java
@@ -1,37 +1,44 @@
-package top.moxingwang.simplemock.test.service;
-
+package top.moxingwang.simplemock.test.mock;
 
 import com.chinaredstar.ordercenter.api.common.OrderResult;
 import top.moxingwang.simplemock.core.annotation.SimpleMock;
+import top.moxingwang.simplemock.test.config.MockConfig;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @SimpleMock
-public class UserService {
+public class MockForReturnTypeService {
+    public static void main(String[] args) {
+        MockConfig.init();
 
-    public Map<String, String> ObjectMethodAdapter() {
+        MockForReturnTypeService mockService = new MockForReturnTypeService();
+        mockService.testMap();
+    }
+
+
+    public Map<String, String> testMap() {
         System.out.println("服务未被mock");
 
         Map<String, String> testMap = new HashMap<>();
         testMap.put("UserService", "UserService测试返回");
-
         return testMap;
     }
 
-    public void VoidMethodAdapter() {
-        System.out.println("服务未被mock");
-    }
-
-    public String StringMethodAdapter() {
+    public String testString() {
         System.out.println("服务未被mock");
         return "OK";
     }
 
-    public OrderResult OrderResult() {
+    public OrderResult testOrderResult() {
         System.out.println("服务未被mock");
         return OrderResult.newSuccess();
     }
+
+    public void testVoid() {
+        System.out.println("服务未被mock");
+    }
+
 
     public int testInt() {
         System.out.println("服务未被mock");
