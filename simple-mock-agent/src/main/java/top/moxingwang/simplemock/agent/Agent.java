3293072a6f55e0003c2f1d70a96d5a05120889bc
xingwang.mo <xingwang.mo@chinaredstar.com>
2019-11-07 22:52:49 +0800

    commit pom

--- simple-mock-agent/src/main/java/top/moxingwang/simplemock/agent/Agent.java
+++ simple-mock-agent/src/main/java/top/moxingwang/simplemock/agent/Agent.java
@@ -4,23 +4,39 @@ package top.moxingwang.simplemock.agent;
 import java.io.File;
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
-import java.net.MalformedURLException;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 public class Agent {
+    static int aa = 1;
+
+    static {
+        aa = aa + 1;
+    }
+
     public static void premain(String agentArgs, Instrumentation inst) {
         System.out.println(" premain(String args, Instrumentation inst) ");
-        inst.addTransformer(new AgentTransformer(0,null,null));
+        inst.addTransformer(new AgentTransformer(0, null, null));
     }
 
     public static void agentmain(String args, Instrumentation inst) {
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaa" + aa);
         {
-            loadClass("C:\\\\workspace\\\\simple-mock\\\\simple-mock-spy\\\\target\\\\simple-mock-spy-1.0.1-SNAPSHOT-jar-with-dependencies.jar",
-                    "org.objectweb.asm.ClassVisitor");
+            try {
+                URL url = new URL("file:C:\\workspace\\simple-mock\\simple-mock-agent\\target\\simple-mock-spy-1.0.1-SNAPSHOT-jar-with-dependencies.jar");
+                URLClassLoader urlClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+                Method add = URLClassLoader.class.getDeclaredMethod("addURL", new Class[]{URL.class});
+                add.setAccessible(true);
+                add.invoke(urlClassLoader, new Object[]{url});
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+
+
         System.out.println(" agentmain(String args, Instrumentation inst) ");
-        inst.addTransformer(new AgentTransformer(1,args,null), true);
+        inst.addTransformer(new AgentTransformer(1, args, null), true);
         try {
             inst.retransformClasses(Class.forName(args));
         } catch (UnmodifiableClassException e) {
@@ -30,19 +46,5 @@ public class Agent {
         }
     }
 
-    static public Class<?> loadClass(String filename, String classname) {
-        try {
-            File file = new File(filename);
-            URL url = file.toURI().toURL();
-            @SuppressWarnings("resource")
-            URLClassLoader loader = new URLClassLoader(new URL[] { url });
-            //
-            return loader.loadClass(classname);
-        } catch (Exception e) {
-            e.printStackTrace();
 
-        }
-
-        return null;
-    }
 }
