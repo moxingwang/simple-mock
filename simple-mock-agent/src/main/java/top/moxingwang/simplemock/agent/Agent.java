9ef5b8b12771b89486a252d91cd9ea6e4efa7b08
xingwang.mo <xingwang.mo@chinaredstar.com>
2019-11-07 18:38:18 +0800

    commit

--- simple-mock-agent/src/main/java/top/moxingwang/simplemock/agent/Agent.java
+++ simple-mock-agent/src/main/java/top/moxingwang/simplemock/agent/Agent.java
@@ -5,18 +5,14 @@ import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
 
 public class Agent {
-
     public static void premain(String agentArgs, Instrumentation inst) {
         System.out.println(" premain(String args, Instrumentation inst) ");
-
         inst.addTransformer(new AgentTransformer());
     }
 
     public static void agentmain(String args, Instrumentation inst) {
         System.out.println(" agentmain(String args, Instrumentation inst) ");
-
         inst.addTransformer(new AgentTransformer(), true);
-
         try {
             inst.retransformClasses(Class.forName(args));
         } catch (UnmodifiableClassException e) {
@@ -25,5 +21,4 @@ public class Agent {
             e.printStackTrace();
         }
     }
-
 }
